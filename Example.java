17c00537a499e9e47500d375b80a9a2a6153585a
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2020-06-02 19:47:16 +0530

    sample window 0.1

---
+++ Example.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+class Example{
+	public static void main(String args[]){
+		JFrame f2=new JFrame();
+		f2.setSize(400,400);
+		f2.setTitle("Add Customer Form");
+		f2.setDefaultCloseOperation(3);
+		f2.setVisible(true);
+	}
+}
